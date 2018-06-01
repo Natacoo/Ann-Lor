ea8203fd17904bcd6ac240af0cfe67ea037f6ac9
Florian <Florian@DESKTOP-HE50T2D>
2018-06-01 10:47:53 +0200

    1.4.3 - SNAPSHOT
    Remplissage des fonctions

--- view/src/main/java/view/Panel.java
+++ view/src/main/java/view/Panel.java
@@ -12,26 +12,35 @@ import javax.swing.JPanel;
 import contract.*;
 
 
-public class Panel extends JPanel implments Observer{
-	
-	private static final long serialVersionUID
+public class Panel extends JPanel implements Observer{
 	
 	public View view;
 	
 	public Panel(View view) {
 		
+		view.setBackground(Color.BLACK);
+		this.view = view;
+		view.getModel().addObserver(this);
 	}
 	
 	public void paintComponent(Graphics g) {
-		
-	}
+		clearComponant(g);
+		for(IElement [] element : view.getModel().getMap().getElements()) {
+			for(IElement e : element) {
+				if(e != null) {
+					e.draw(g);
+				}
+			}
+		}
 	
 	public void clearComponant (Graphics g) {
-		
+		g.setColor(Color.BLACK);
+		g.fillRect(0,  0, view.getWidth(), view.getHeight());
 	}
 	
 	public void update(Observable observable, Object object) {
-		
+		repaint();
 	}
 	
+	}
 }
--- view/src/main/java/view/View.java
+++ view/src/main/java/view/View.java
@@ -8,40 +8,49 @@ import javax.swing.JFrame;
 import contract.*;
 
 
-public classe View extends JFrame implements IView, KeyListener{
+public class View extends JFrame implements IView, KeyListener{
+	
 	private String title;
 	private IController controller;
 	private IModel model;
 	
 	
 	public View(IModel model, String title) {
+		this.model = model;
+		addKeyListener(this);
+		setTitle(title);
+		setSize(1280, 640);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setContentPane(new Panel(this));
+		setResizable(false);
+		setVisible(true);
 		
 	}
 	
 	public IModel getModel() {
-		
+		return model;
 	}
 	
 	public void setController(IController c) {
-		
+		controller = c;
 	}
 	
 	protected IController getController() {
-		
+		return controller;
 	}
 	
 	public void setTitle(String title) {
-		
+		this.title = title;
 	}
 	
 	public String getTitle () {
-		
+		return title;
 	}
 	
 	public void keyTyped(KeyEvent keyEvent) {}
 	
 	public void keyPressed(KeyEvent keyEvent) {
-		
+		controller.keyEvent(keyEvent.getKeyCode());
 	}
 	
 	public void keyReleased(KeyEvent keyEvent) {}
