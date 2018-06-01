f106aaeb772f38455168b5d36485eb088b9b3faa
Florian <Florian@DESKTOP-HE50T2D>
2018-06-01 10:29:05 +0200

    1.4.2 - SNAPSHOT
    Implémentation des fonctions et du controller

--- view/src/main/java/view/Panel.java
+++ view/src/main/java/view/Panel.java
@@ -9,3 +9,29 @@ import java.util.Observable
 
 import javax.swing.JPanel;
 
+import contract.*;
+
+
+public class Panel extends JPanel implments Observer{
+	
+	private static final long serialVersionUID
+	
+	public View view;
+	
+	public Panel(View view) {
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+	}
+	
+	public void clearComponant (Graphics g) {
+		
+	}
+	
+	public void update(Observable observable, Object object) {
+		
+	}
+	
+}
--- view/src/main/java/view/View.java
+++ view/src/main/java/view/View.java
@@ -1,5 +1,49 @@
 package view;
 
-public class View {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+import javax.swing.JFrame;
+
+import contract.*;
+
+
+public classe View extends JFrame implements IView, KeyListener{
+	private String title;
+	private IController controller;
+	private IModel model;
+	
+	
+	public View(IModel model, String title) {
+		
+	}
+	
+	public IModel getModel() {
+		
+	}
+	
+	public void setController(IController c) {
+		
+	}
+	
+	protected IController getController() {
+		
+	}
+	
+	public void setTitle(String title) {
+		
+	}
+	
+	public String getTitle () {
+		
+	}
+	
+	public void keyTyped(KeyEvent keyEvent) {}
+	
+	public void keyPressed(KeyEvent keyEvent) {
+		
+	}
+	
+	public void keyReleased(KeyEvent keyEvent) {}
 }
+
